b71c7c75178fc2716783477d2d28a5c9d0397346
hernan <hernancloud02@gmail.com>
2021-01-01 16:27:08 -0400

    BeforeEach and AfterEach

--- src/test/java/com/example/demo/suma/SumaTest.java
+++ src/test/java/com/example/demo/suma/SumaTest.java
@@ -3,7 +3,9 @@ package com.example.demo.suma;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,17 +21,34 @@ public class SumaTest {
 		log.info("BeforeAll");
 	}
 
+	@BeforeEach
+	public void beforeEach() {
+		log.info("beforeEach");
+	}
+
+	@AfterEach
+	public void afterEach() {
+		log.info("AfterEach");
+	}
+
 	@Test
-	public void sumaTest() {
-		log.info("sumaTest");
+	public void sumaTest1() {
+		log.info("sumaTest-1");
 		int resultTest = suma.suma(1, 1);
 		int expectedResult = 2;
 		assertEquals(expectedResult, resultTest);
 	}
 
+	@Test
+	public void sumaTest2() {
+		log.info("sumaTest-2");
+		int resultTest = suma.suma(1, 0);
+		int expectedResult = 1;
+		assertEquals(expectedResult, resultTest);
+	}
+
 	@AfterAll
 	public static void afterAll() {
-		System.out.println("AfterAll");
 		log.info("AfterAll");
 	}
 
