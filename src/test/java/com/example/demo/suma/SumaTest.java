81817e793aaf04b86a055397f0dfad708b732aff
hernan <hernancloud02@gmail.com>
2021-01-01 16:21:10 -0400

    BeforeAll and AfterAll

--- src/test/java/com/example/demo/suma/SumaTest.java
+++ src/test/java/com/example/demo/suma/SumaTest.java
@@ -2,17 +2,35 @@ package com.example.demo.suma;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SumaTest {
 
+	private static final Logger log = LoggerFactory.getLogger(SumaTest.class);
+
 	Suma suma = new Suma();
+
+	@BeforeAll
+	public static void beforeAll() {
+		log.info("BeforeAll");
+	}
 
 	@Test
 	public void sumaTest() {
+		log.info("sumaTest");
 		int resultTest = suma.suma(1, 1);
 		int expectedResult = 2;
 		assertEquals(expectedResult, resultTest);
+	}
+
+	@AfterAll
+	public static void afterAll() {
+		System.out.println("AfterAll");
+		log.info("AfterAll");
 	}
 
 }
